35712c828513534119cbaf3db030a9cbbfa353f7
Andrew Park <andysampark@gmail.com>
2022-10-11 03:00:27 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,109 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [exactPhrases, setExactPhrases] = useState([
+    "internship",
+    "undergraduate",
+  ]);
+
+  function handleKeyDown(e) {
+    if (e.key === "Enter") {
+      console.log("do validate");
+      setExactPhrases((oldArray) => [...oldArray, e.target.value]);
+      console.log({ exactPhrases });
+    }
+  }
+
+  function handleClick(index) {
+    console.log(index);
+    setExactPhrases((oldArray) =>
+      exactPhrases.filter((phrase) => phrase != exactPhrases[index])
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex flex-col	w-full items-center justify-center min-h-screen text-center my-auto">
+      <div className="flex flex-row gap-10 my-10">
+        <div className="flex flex-col justify-start items-start">
+          <label>Exact Phrases</label>
+          <input
+            type="text"
+            placeholder="Type here"
+            class="input w-full max-w-xs"
+            onKeyDown={handleKeyDown}
+          />
+        </div>
+        <div className="flex flex-col justify-start items-start">
+          <label>Excluded Words</label>
+          <input
+            type="text"
+            placeholder="Type here"
+            class="input w-full max-w-xs"
+            onKeyDown={handleKeyDown}
+          />
+        </div>
+      </div>
+      {/* <div className="flex flex-col justify-start items-start">
+          <label>Excluded Words</label>
+          <input
+            type="text"
+            placeholder="Type here"
+            class="input w-full max-w-xs"
+            onChange={(e) => setSpecificPhrase(e.target.value)}
+          />
+        </div>
+        <div className="flex flex-col justify-start items-start">
+          <label>Specific Sites</label>
+          <input
+            type="text"
+            placeholder="Type here"
+            class="input w-full max-w-xs"
+            onChange={(e) => setSpecificPhrase(e.target.value)}
+          />
+        </div>
+        <div className="flex flex-col justify-start items-start">
+          <label>Similar</label>
+          <input
+            type="text"
+            placeholder="Type here"
+            class="input w-full max-w-xs"
+            onChange={(e) => setSpecificPhrase(e.target.value)}
+          />
+        </div>
+        <div className="flex flex-col justify-start items-start">
+          <label>Before</label>
+          <input
+            type="text"
+            placeholder="Type here"
+            class="input w-full max-w-xs"
+            onChange={(e) => setSpecificPhrase(e.target.value)}
+          />
+        </div>
+        <div className="flex flex-col justify-start items-start">
+          <label>After</label>
+          <input
+            type="text"
+            placeholder="Type here"
+            class="input w-full max-w-xs"
+            onChange={(e) => setSpecificPhrase(e.target.value)}
+          />
+        </div> */}
+
+      <div className="flex flex-row">
+        {exactPhrases.map((exactPhrase, index) => {
+          return (
+            <p
+              key={index}
+              className="hover:cursor-pointer"
+              onClick={(e) => handleClick(index)}
+            >
+              "{exactPhrase}"&nbsp;
+            </p>
+          );
+        })}
+        {/* <h1>{exactPhrases[1]}</h1> */}
+      </div>
     </div>
   );
 }
